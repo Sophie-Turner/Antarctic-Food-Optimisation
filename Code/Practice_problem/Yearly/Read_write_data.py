2f93810ed3f3ab8247ceaf2898d5dedfa8eabba9
Sophie Turner <st838@cam.ac.uk>
2022-06-03 12:32:29 +0100

    Found problems in masterPAX data. Sanitise in Python.

--- Code/Practice_problem/Yearly/Read_write_data.py
+++ Code/Practice_problem/Yearly/Read_write_data.py
@@ -5,16 +5,47 @@ import numpy as np
 highIntensityJobs = ["scien", "boat", "chef", "tech", "mech", "elec", "plumb", "weld"] 
 # Large increase in food required for these people. + 100%
 vHighIntensityJobs = ["carpenter", "builder", "joiner", "construction", "field", "dive", "ga", "marine biologist", "erector", "pilot", "air", "traverse", "cladder", "mooring", "hut install"]
-
 # File path of master pax sheet.
 paxPath = r"C:\Users\sophi\Antarctic-Food-Optimisation\Data\MasterPAX.xlsx"
-sheet = pd.read_excel(paxPath)
 
+
+def findInvalidIndices(dates):
+    # Detect string dates. They are things like year 20211. 
+    invalidIndices = []
+    for i in range(len(dates)):
+        date = dates[i]
+        if type(date) == str:
+            invalidIndices.append(i)
+    return invalidIndices
+
+
+def sanitise(dataArray, invalidIndices):
+    # Also remove records for this person as it is all TBC.
+    i = 0
+    for index in invalidIndices:
+        dataArray = np.delete(dataArray, index-i)
+        i += 1
+    return dataArray     
+
+
+sheet = pd.read_excel(paxPath, na_filter = False)
 posts = np.asarray(sheet.iloc[2:373, 8])
 types = np.asarray(sheet.iloc[2:373, 9])
-jobsIntensity = np.zeros(len(posts))
+startDates = np.asarray(sheet.iloc[2:373, 1])
+endDates = np.asarray(sheet.iloc[2:373, 3])
 
-for i in range(len(posts)):
+invalidIndices = findInvalidIndices(startDates)
+
+posts = sanitise(posts, invalidIndices)
+types = sanitise(types, invalidIndices)
+startDates = sanitise(startDates, invalidIndices)
+endDates = sanitise(endDates, invalidIndices)
+
+totalPeople = len(posts)
+print(totalPeople)
+jobsIntensity = np.zeros(totalPeople)
+
+for i in range(totalPeople):
     post = posts[i]
     # If it contains a value it will be a string.
     if type(post) == float:
@@ -28,13 +59,12 @@ for i in range(len(posts)):
             if job in post:
                 jobsIntensity[i] = 1
 
-print(jobsIntensity)
+firstDay = min(startDates) 
+lastDay = max(endDates)
 
-# How many people are on base each day.
-#numPeople = np.asarray(sheet.iloc[374, 13:376])
-#print(sheet.iloc[374, 13])
-#print(sheet.iloc[362, 7])
-print(sheet.iloc[374, 1:50])
-#print(sheet.iloc[374, 376])
-print(sheet.shape)
+print(firstDay, lastDay)
+
+numPeople = []
+for person in range(totalPeople):
+    pass
 
